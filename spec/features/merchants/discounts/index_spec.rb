8a4a0e059169a2477a143359ce1a8ea3357adba8
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-10 08:21:19 -0700

    Implement discount index page, modify tests

--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -7,22 +7,27 @@ RSpec.describe "merchant discounts index page" do
     @discount_1 = create(:discount, merchant: @merchant_1, percentage_discount: 0.2, quantity_threshold: 10)
     @discount_2 = create(:discount, merchant: @merchant_1, percentage_discount: 0.3, quantity_threshold: 15)
     @discount_3 = create(:discount, merchant: @merchant_1, percentage_discount: 0.4, quantity_threshold: 20)
-    @discount_4 = create(:discount, merchant: @merchant_2, percentage_discount: 0.4, quantity_threshold: 20)
+    @discount_4 = create(:discount, merchant: @merchant_2, percentage_discount: 0.7, quantity_threshold: 50)
   end
 
   it "shows all this merchant's discounts and their details" do
     visit merchant_discounts_path(@merchant_1)
 
-    expect(page).to have_content(@discount_1.percentage_discount)
-    expect(page).to have_content(@discount_1.quantity_threshold)
-    expect(page).to have_content(@discount_2.percentage_discount)
-    expect(page).to have_content(@discount_2.quantity_threshold)
+    within("#discount-#{@discount_1.id}") do
+      expect(page).to have_content(@discount_1.percentage_discount)
+      expect(page).to have_content(@discount_1.quantity_threshold)
+    end
+
+    within("#discount-#{@discount_2.id}") do
+      expect(page).to have_content(@discount_2.percentage_discount)
+      expect(page).to have_content(@discount_2.quantity_threshold)
+    end
   end
 
   it "does not show discounts from any other merchants" do
     visit merchant_discounts_path(@merchant_1)
 
-    expect(page).to_not have_content(@discount_3.percentage_discount)
-    expect(page).to_not have_content(@discount_3.quantity_threshold)
+    expect(page).to_not have_content(@discount_4.percentage_discount)
+    expect(page).to_not have_content(@discount_4.quantity_threshold)
   end
 end
