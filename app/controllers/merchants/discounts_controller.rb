f84ef5a7baa36ada9b2dd908c2498b63465bb3b7
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-10 12:35:46 -0700

    Finalize merchant discount edit feature

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -38,7 +38,14 @@ class Merchants::DiscountsController < ApplicationController
   end
 
   def update
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.find(params[:id])
+    if @discount.update(discount_params)
+      redirect_to merchant_discount_path(@merchant, @discount)
+    else
+      redirect_to edit_merchant_discount_path(@merchant, @discount)
+      flash[:alert] = "Error: #{error_message(@discount.errors)}"
+    end
   end
 
   private
