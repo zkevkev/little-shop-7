ee79f925db5b0eaeba10082021cf5f589f5f4772
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-10 11:53:57 -0700

    Refactor specs for merchant discount show, needs percentage formatting method

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -12,19 +12,19 @@ RSpec.describe "merchant discount show page" do
     
     expect(page).to have_content(@merchant_1.name)
     within("#id") do
-      expect(page).to have_content(@discount_1.id)
+      expect(page).to have_content("ID: #{@discount_1.id}")
     end
     within("#percentage_discount") do
-      expect(page).to have_content(@discount_1.percentage_discount)
+      expect(page).to have_content("Percentage Discount: 20%")
     end
     within("#quantity_threshold") do
-      expect(page).to have_content(@discount_1.quantity_threshold)
+      expect(page).to have_content("Quantity Threshold: #{@discount_1.quantity_threshold}")
     end
   end
 
   it "does not show information from other discounts" do
     visit merchant_discount_path(@merchant_1, @discount_1)
-    
+
     expect(page).to_not have_content(@discount_2.id)
     expect(page).to_not have_content(@discount_2.percentage_discount)
     expect(page).to_not have_content(@discount_2.quantity_threshold)
