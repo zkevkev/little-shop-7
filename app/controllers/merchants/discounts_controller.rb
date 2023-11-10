5b4f2b5df2bf921b21cfd6f36050793f74bc8506
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-10 08:57:12 -0700

    Write merchant discounts controller new and create actions

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -8,4 +8,24 @@ class Merchants::DiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path(merchant)
+    else
+      redirect_to new_merchant_discount_path
+      flash[:alert] = "Error: #{error_message(discount.errors)}"
+    end
+  end
+
+  private
+  
+  def discount_params
+    params.permit(:percentage_discount, :quantity_threshold, :merchant_id)
+  end
 end
